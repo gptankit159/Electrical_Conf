cab615f000931ec3cba7f8b0ea694e5efcfb54fc
Ankit Gupta <gptankit159@gmail.com>
2023-02-08 22:42:40 +0530

    Initial Commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>ITSEMC-2023</title>
+ <?php $page = 'home'; include('styles.php') ?>
+ 
+
+ <!-- Intelligent Technologies for Sustainable EnergyManagement and Control (ITSEMC-2023) -->
+<body>
+<?php include('navnheader.php') ?>
+<?php include('home.php') ?>
+<?php include('footer.php') ?>
+
+</body>
+
+</html>
