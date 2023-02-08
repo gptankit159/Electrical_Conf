0679e43b1c546948fba969675f9d1894ced72fb9
Ankit Gupta <gptankit159@gmail.com>
2023-02-08 23:07:19 +0530

    Done with call for papers

---
+++ callForPapers.php
@@ -0,0 +1,282 @@
+<!doctype html>
+
+<html lang="en">
+
+
+
+<head>
+
+  <!-- Required meta tags -->
+
+  <meta charset="utf-8">
+
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>Call For Papers</title>
+
+ <?php $page = 'callForPapers'; include('styles.php') ?>
+
+ 
+
+</head>
+
+
+
+<body>
+
+<?php include('navnheader.php') ?>
+
+
+
+<!-- Content  -->
+
+
+
+
+
+<div class="container-fluid pad-0">
+
+    <div class="banner-inner text-center">
+
+      <h3><strong> Call for Papers</strong></h3>
+
+    </div>
+
+  </div>
+
+  <div class="whitespace40">&nbsp;</div>
+
+  <div style="max-width:1000px; margin:auto; font-size:1.05rem;"  class="container middle-content ">
+
+    <p>
+    All papers must be original and not simultaneously submitted to another journal or conference. The following paper categories are welcome: 
+    </p>
+
+    <div class="whitespace15">&nbsp;</div>
+
+    <h4>Discussion <span>Topics</span></h4>
+
+    <div class="whitespace15">&nbsp;</div>
+
+   
+
+    <p><b>Topics of interest include, but we are not limited to, the following</b> </p>
+
+    <h5>Track-1: Power electronic application to power system  </h5>
+    <ul class="list_inner">
+    <li> Electrical Machines and Drives </li>
+    <li> High Voltage Engineering & Technology </li>
+    <li> Power Electronics and Applications </li>
+    <li> Integration of Distributed Energy Resources </li>
+    <li> Power System Economics and Energy Markets </li>
+    <li> Power System Planning and Operation </li>
+    <li> Renewable Energy Technologies and Issues  </li>
+    <li> Smart Grid Technologies and Issues  </li>
+    <li> Industrial Electronics </li>
+    <li> Industrial Applications </li>
+    <li> Control, Intelligent Systems, Robotics </li>
+    <li> Other related topics </li>
+    </ul>
+
+    <h5>Track-2: Modelling and Optimization</h5>
+      <ul class="list_inner">
+        <li>High Performance Computing and Simulation</li>
+        <li>Mathematical and Numerical methods</li>
+        <li>Real-time modeling and simulation</li>
+        <li>Simulation Application in Industry</li>
+        <li>Statistical and Probabilistic Modeling</li>
+        <li>Mathematical and Numerical methods</li>
+        <li>IoT modeling and simulation</li>
+        <li>Mathematical and Numerical methods</li>
+        <li>Simulation Application in Industry</li>
+        <li>Adaptive and intelligent control</li>
+        <li>Artificial intelligence</li>
+        <li>Modelling of energy system</li>
+      </ul>
+
+    <h5>Track-3: IoT and Communications</h5>
+      <ul class="list_inner">
+        <li>IoT architecture design and optimization</li>
+        <li>Security and privacy of IoT devices and services</li>
+        <li>System optimization to support ultra-low complexity devices</li>
+        <li>Standardised semantic data description framework and technologies</li>
+        <li>IoT communication procedure enhancements</li>
+        <li>Experience and lessons learntforstandards based IoT large scale pilots</li>
+        <li>IoT standards for platform interworking</li>
+        <li>IoT interoperability methodologies</li>
+        <li>IoT standards gap analysis</li>
+        <li>5G Networks and IoT</li>
+        <li>Low Power Wide Area (LPWA) networks</li>
+        <li>Software Defined Network (SDN) and IoT</li>
+        <li>Industrial Internet of Things</li>
+        <li>Internet of Mobile Things (IoMT) architecture design and protocols</li>
+        <li>Massive data transportation and analysis through IoT</li>
+        <li>Factory of Things</li>
+        <li>Edge Computing, Fog Computing, Cloud Computing and IoT</li>
+        <li>IPv6-based IoT Networks</li>
+        <li>IoT communication protocols: IPv6, 6LoWPAN, RPL, 6TiSCH, WoT, LoRaWAN, NB-IoT</li>
+        <li>IoT data protocol: MQTT-SN, COAP, XMPP-IoT, AMQP</li>
+        <li>IoT security in the sense of massive IoT deployments</li>
+        <li>URLLC for mission critical IoT</li>
+        <li>V2X standards and architectures</li>
+
+      </ul>
+
+    <h5>Track-4 : Materials and Management</h5>
+        <ul class="list_inner">
+         <li>Nanomaterial for Energy & Environment</li>
+         <li>Advanced functional and structural material</li>
+         <li>Materials Science In Construction Engineering</li>
+         <li>Metals and Metallurgy</li>
+         <li>Nano Technology & Nano materials</li>
+         <li>Material Science and Engineering</li>
+         <li>Future of Materials</li>
+         <li>Advanced Materials & Material Chemistry</li>
+         <li>Empirical Research in Operations Management</li>
+         <li>Operations Management</li>
+         <li>Closed Loop Supply Chain management</li>
+         <li>Healthcare Operations Management</li>
+         <li>Information in Operations Management</li>
+         <li>Product Innovation and Technology Management</li>
+         <li>Production Planning and Scheduling</li>
+         <li>Purchasing and Supply Management</li>
+         <li>Risk Management in Operations</li>
+         <li>Sustainable Operations</li>
+
+        </ul>
+
+    <h5>Track-5 : Energy: Generation, Transmission and Distribution</h5>
+        <ul class="list_inner">
+         <li>Energy, Environment & Decarbonization</li>
+         <li>Developments in the Global Energy Transition</li>
+         <li>Digitalization in energy development and power generation</li>
+         <li>Operational & Economic Aspect of Power Generation</li>
+         <li>Generation Planning, Electricity Policies, and Regulation</li>
+         <li>Power Generation & Renewables</li>
+         <li>Green Energy Conversion Systems</li>
+         <li>Distributed Storage & Generation Systems</li>
+         <li>Renewable Integration & Operation</li>
+         <li>Residential Renewable Generation</li>
+         <li>Development in Energy Storage Systems</li>
+         <li>Power Plant Monitoring, Protection Control, and Automation</li>
+         <li>Power Plant Management & Maintenance</li>
+         <li>Reliable & Affordable Access to Energy Sources</li>
+         <li>Advancements in Power Transmission & Distribution</li>
+         <li>HVDC & Flexible AC Transmission System</li>
+         <li>Future Distribution Network Planning & Electrification</li>
+         <li>Microgrids Integration & Operation</li>
+         <li>Impact of Climate Change on T&D</li>
+         <li>T&D Monitoring, Protection, Automations, and Restoration</li>
+         <li>Hybrid Distribution Networks and Power Electronics</li>
+         <li>Ancillary Services and Resiliency in LV Distribution Networks</li>
+         <li>Reducing Distribution Losses</li>
+         <li>Power Quality Issues Associated with Microgrid</li>
+         <li>Electricity Market Design & Regularities</li>
+         <li>Financing Power and Energy Investment</li>
+
+        </ul>
+
+    <h5>Track-6 : Electric Mobility and Transportation</h5>
+        <ul class="list_inner">
+         <li>Power Electronics for Hybrid and Electric Vehicles</li>
+         <li>Vehicles Power Electronics Circuit and System</li>
+         <li>Charging System and Standards</li>
+         <li>Small, Medium and Long-range Wireless Power Transfer</li>
+         <li>Battery management system</li>
+         <li>Electric vehicle security</li>
+         <li>Renewable energy for electric vehicles</li>
+         <li>Modelling, simulation, and control for vehicles</li>
+        </ul>
+
+    <h5>Track-7 : Energy Efficient Buildings</h5>
+        <ul class="list_inner">
+         <li>Critical Infrastructures and Their Security</li>
+         <li>Decarbonization Strategies for Smart buildings</li>
+         <li>Sustainable Energy</li>
+         <li>Building Energy Codes and Standards</li>
+         <li>Relevant Policies at the National & Sub National Level</li>
+         <li>Thermal comfort</li>
+         <li>The embodied energy of construction materials and technologies</li>
+         <li>Role of building energy simulation</li>
+         <li>Climate responsive design</li>
+         <li>Building technologies and controls</li>
+         <li>Space cooling and low energy cooling technologies</li>
+         <li>Net-zero buildings and neighbourhoods</li>
+         <li>Behavioural and socio-cultural aspects affecting energy use</li>
+         <li>Building Energy Performance</li>
+         <li>Standards and Labelling of Appliances</li>
+         <li>Super-Efficient Appliance</li>
+         <li>Advanced structural materials</li>
+
+        </ul>
+
+    <h5>Track-8 : Data Science and Machine Learning</h5>
+        <ul class="list_inner">
+         <li>Benefits of Data Analytics</li>
+         <li>Volume Growth of Analytic Data</li>
+         <li>Managing Data Analytics</li>
+         <li>Data Analysis</li>
+         <li>Data Manipulation</li>
+         <li>Data Cleansing</li>
+         <li>Cluster Analysis</li>
+         <li>Debugging</li>
+         <li>Human Intelligence</li>
+         <li>Supervised learning</li>
+         <li>Machine Learning Python Libraries</li>
+         <li>Machine Learning Engineering Stack</li>
+         <li>Database Mining and Statistical Analysis</li>
+         <li>Advanced Machine Learning</li>
+         <li>Convolutional Neural Network</li>
+         <li>Artificial Neural Network</li>
+         <li>Python Programming</li>
+         <li>Multi-Task Learning</li>
+         <li>Facial Recognition System</li>
+         <li>Data Exploration and Explanation</li>
+         <li>Barriers to Data Analytics</li>
+         <li>Data Visualization and Presentation</li>
+
+        </ul>
+
+    <h5>Track-9 : Emerging Trends in Engineering for Sustainability</h5>
+        <ul class="list_inner">
+         <li>Wireless Communication and Sensor Networks</li>
+         <li>Pattern Recognition and Image Understanding</li>
+         <li>Blockchain, Inter-Vehicle Wireless Communications Technologies</li>
+         <li>High-Performance Computing, Cloud Computing</li>
+         <li>Computational Intelligence and Evolutionary Computation</li>
+         <li>Renewable and Sustainable Energy Solutions</li>
+         <li>Image Processing and Computational Intelligence in Agriculture</li>
+         <li>Technology in Agriculture for Sustainable Development</li>
+         <li>Green Technology and Rural Environmental Concerns</li>
+         <li>Sustainable Manufacturing through IT Enabled Technologies</li>
+         <li>Technological Forecasting and Social Change</li>
+         <li>Energy management and Environmental Engineering</li>
+         <li>Big Data Acquisition, Integration, Cleaning, and Best Practices</li>
+         <li>Next Generation Networks</li>
+         <li>Natural Language Processing</li>
+         <li>Advanced structural materials</li>
+
+        </ul>
+
+    </div>
+    
+
+
+    <div class="whitespace15">&nbsp;</div>
+    <!-- Content  -->
+
+
+
+
+  <?php include('footer.php') ?>
+
+  
+
+  
+
+</body>
+
+
+
+</html>
+
